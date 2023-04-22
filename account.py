49aa215a82a26786080dc6938149bedb8641f40e
danielmaher4277 <131501940+danielmaher4277@users.noreply.github.com>
2023-04-21 21:34:05 -0500

    Update account.py

--- account.py
+++ account.py
@@ -1,28 +1,25 @@
 class Account():
-    def __init__(self, name):
-        self.__account_name = name
+    def __init__(self, name: str):
+        self.__account_name = name  # initializing private name variable
 
-        self.__account_balance = 0
+        self.__account_balance = 0.0  # initializing private account balance variable
 
-    def deposit(self, amount):
+    def deposit(self, amount: float):
         if amount > 0:
-            self.__account_balance += amount
+            self.__account_balance += amount  # adds to account balance if greater then 0
             return True  # Transaction successful
         else:
             return False  # Transaction unsuccessful
 
-    def withdraw(self, amount):
+    def withdraw(self, amount: float):
         if (amount > 0) and (amount <= self.__account_balance):
-            self.__account_balance -= amount
+            self.__account_balance -= amount  # Withdraws amount from account if not a negative number or greater then account balance.
             return True  # Transaction successful
         else:
             return False  # Transaction unsuccessful
 
-    def get_balance(self):
+    def get_balance(self):  # Returns final value of account balance
         return self.__account_balance
 
-    def get_name(self):
+    def get_name(self):  # Returns initial name variable
         return self.__account_name
-
-
-
